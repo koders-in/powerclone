86d62221cb2adb705507d6788b020cbd2b2c1f2a
Kartikey Kushwah <kushwah.kartikey14@stu.upesalumni.ac.in>
2022-10-17 12:53:45 +0530

    Added timer

--- powerclone.sh
+++ powerclone.sh
@@ -1,12 +1,11 @@
-#!bin/bash
+#!/bin/bash
 
 # TODO => Update pulls on each branches if possible
 
 start_time=$(date +%s)
-cd "$(dirname "$0")"  # Setting current directory to the calling folder
+cd "$PWD"
 for remote in $@ 
 do
-  # pwd
   basename=$(basename $remote)
   reponame=${basename%.*}
   mkdir -p $reponame
@@ -27,5 +26,5 @@ do
 done
 end_time=$(date +%s)
 elapsed=$(( end_time - start_time ))
-eval "echo Powercloned in: $(date -ud "@$elapsed" +'$((%s/3600/24)) days %H hr %M min %S sec')"
+echo "Powercloned in $elasped seconds"
 
